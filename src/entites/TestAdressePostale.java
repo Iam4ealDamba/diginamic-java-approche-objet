53180a7cf3d4b3f99aee1c76eaa35361632e1439
Iam4ealDamba <prayforsouls720@gmail.com>
2024-05-03 14:54:05 +0200

    ajout exo operations

--- src/entites/TestAdressePostale.java
+++
@@ -1,9 +0,0 @@
-package entites;
-
-public class TestAdressePostale {
-  public static void main(String[] args) {
-    AdressePostale adresse1 = new AdressePostale("10", "rue de la paix", "75000", "Paris");
-
-    AdressePostale adresse2 = new AdressePostale("12", "rue de la paix", "75000", "Paris");
-  }
-}
