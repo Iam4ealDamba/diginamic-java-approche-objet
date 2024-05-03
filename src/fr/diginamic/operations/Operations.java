d7576c8cb77acacb1fa5d4db74319d1a25a81a6b
Iam4ealDamba <prayforsouls720@gmail.com>
2024-05-03 15:18:30 +0200

    ajout exo

---
+++ src/fr/diginamic/operations/Operations.java
@@ -0,0 +1,16 @@
+package fr.diginamic.operations;
+
+public class Operations {
+  public double calcul(double a, double b, char operator) {
+    switch (operator) {
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return a / b;
+      default:
+        return a + b;
+    }
+  }
+}
